4be8bb362414eaca21e9fbce60ba23884ada8b9e
Luis Alvarez <lalvarezguillen@gmail.com>
2019-11-08 19:36:27 +0000

    adopted marshmallow and cleaned up

--- handler.py
+++ handler.py
@@ -2,59 +2,57 @@ import json
 import os
 from urllib.parse import parse_qs
 
-from email_validator import validate_email, EmailNotValidError
+from marshmallow import Schema, fields, post_load
 
 
-DEV_NAME = os.environ.get('dev_name', 'default dev name')
-
-
-class FormData:
+class Contact:
+    '''
+    Represents a Contact event. A submission of the
+    Contact form.
+    '''
     def __init__(self, email: str, name: str, content: str, **kwargs):
         self.email = email
         self.name = name
         self.content = content
 
 
-class ValidationError(Exception):
+class ContactSchema(Schema):
     '''
-    To be raised when the form data is not valid
+    Valiates the content of a Contact Form submission.
     '''
+    email = fields.Email(required=True)
+    name = fields.String(required=True)
+    content = fields.String(required=True)
 
-def is_email(text: str) -> bool:
-    return '@' in text and '.' in text
-
-
-def parse_form_data(form_data: str) -> FormData:
-    parsed = parse_qs(form_data)
-    fields = ['email', 'name', 'content']
-    for field in fields:
-        if not parsed.get(field):
-            raise ValidationError(f'{field} is required')
-    
-    try:
-        validate_email(parsed['email'][0], check_deliverability=False)
-    except EmailNotValidError:
-        raise ValidationError(f'Invalid email')
-    
-    return FormData(
-        parsed['email'][0],
-        parsed['name'][0],
-        parsed['content'][0]
-    )
+    @post_load
+    def make_contact(self, data, **kwargs):
+        return Contact(**data)
 
 
-def hello(event, context):
-    body = event.get('body', {})
+def parse_qs_single_value(data: str) -> dict:
+    '''
+    Parses a form-encoded string, extracting only one value
+    per field.
+    '''
+    raw = parse_qs(data)
+    return {key: value[0] for key, value in raw.items()}
 
-    try:
-        form_data = parse_form_data(body)
-    except ValidationError as err:
+
+def handle_contact(event, context):
+    '''
+    Handles submissions of the Contact Us form.
+    '''
+    body = parse_qs_single_value(event.get('body', ''))
+    schema = ContactSchema()
+    errors = schema.validate(body)
+    if errors:
         return {
             'statusCode': 400,
-            'body': json.dumps({'error': str(err)})
+            'errors': errors,
         }
 
+    contact = schema.load(body)
     return {
         "statusCode": 200,
-        "body": json.dumps(form_data.__dict__)
+        "body": schema.dump(contact)
     }
